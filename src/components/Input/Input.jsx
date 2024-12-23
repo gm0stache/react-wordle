7112fda8e18199ec57234369555ccb0635110e85
Gabriel <45315755+gm0stache@users.noreply.github.com>
2024-12-23 14:19:59 +0100

    feat: add input validation

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,21 +1,24 @@
 import { React, useState } from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
 
 function Input({ setWord }) {
   const [word, setWordTemp] = useState("");
 
   return (
     <form
-      class="guess-input-wrapper"
+      className="guess-input-wrapper"
       onSubmit={(e) => {
         e.preventDefault();
         setWord(word);
         setWordTemp("");
       }}
     >
-      <label for="guess-input">Enter guess:</label>
+      <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
+        minLength="1"
+        maxLength={NUM_OF_GUESSES_ALLOWED}
         onChange={(e) => setWordTemp(e.target.value)}
         value={word}
       />
